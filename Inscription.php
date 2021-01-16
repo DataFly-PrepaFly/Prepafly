fe8762d6fdb3723e208dbf621e8bc32f52edb07b
loufa12 <76210225+loufa12@users.noreply.github.com>
2021-01-16 14:32:53 +0100

    Modifs Page Inscription

--- Inscription.php
+++ Inscription.php
@@ -6,17 +6,8 @@
     <link rel="stylesheet" href="style/nav_simple.css" />
     <link rel="stylesheet" href="style/footer.css" />
 <head>
-<title>S'inscrire</title>
+<title>PrepaFly - Inscription</title>
 
-<script type="text/javascript">
-    function validerInscri() {
-        if (document.forms["inscri"]["mdp"].value !== document.forms["inscri"]["mdp2"].value) {
-            alert("Le mot de passe n'est pas correctement confirmé");
-            return false;
-        }
-    }
-
-</script>
 </head>
 
 <header>
@@ -30,57 +21,76 @@
 <section>
     <br><h3>INSCRIPTION</h3>
     <div class="texte">
-        <p>Si vous êtes intéressé et souhaitez être recontacté par nos équipes, il vous suffit de remplir ce formulaire.</p>
+        <p>Si vous êtes intéressé et souhaitez être recontacté par nos équipes, il vous suffit de remplir ce formulaire.</p><br>
     </div>
     
-    <div class="zoneinscri">
-        <form action="cible_form.php" method="post" enctype="multipart/form-data" id="form" onsubmit="return validerInscri()">
-            <label for="nfam">Nom : </label> <br>
-            <input type="text" id="nom" name="nom" required size="40"><br><br>
+        <form action="traitement_inscri.php" method="post" enctype="multipart/form-data" id="form">
+            <div id="formulaire">
+                <div id="form_part1">
+                    <div class="champs1">
+                        <label for="nfam">Nom : </label>
+                        <input type="text" id="nom" name="nom" required size="30"><br><br>
+                    </div>
+                    <div class="champs1">
+                        <label for="pren">Prénom : </label>
+                        <input type="text" id="prenom" name="prenom" required size="30"><br><br>
+                    </div>
+                    <div class="champs1">
+                        <label for="pren">Date de naissance : </label>
+                        <input type="date" id="date" name="date" required><br><br>
+                    </div>
+                    <div class="champs1">
+                        <label for="mail">Mail personnel : </label>
+                        <input type="text" name="mail" size="50"><br><br>
+                    </div>
+                    <div class="champs1">
+                        <label for="mailpro">Mail professionnel : </label><br>
+                        <input type="text" name="mailpro" size="50"><br><br>
+                    </div>
+                    <div class="champs1">
+                        <label for="num">Numéro de téléphone : </label>
+                        <input type="text" name="num" size="20"><br><br>
+                    </div>
+                </div>
 
-            <label for="pren">Prénom : </label><br>
-            <input type="text" id="prenom" name="prenom" required size="40"><br><br>
+                <div id="form_part2">
+                    <div class="champs2">
+                        <label for="cpgn">Compagnie aérienne : </label>
+                        <select>
+                            <option value="airchina">Air China</option>
+                            <option value="aircorse">Air Corsica</option>
+                            <option value="airfrance">Air France</option>
+                            <option value="american">American Airlines</option>
+                            <option value="easy">Easy Jet</option>
+                            <option value="emirates">Emirates</option>
+                            <option value="iberia">Iberia</option>
+                            <option value="klm">KLM</option>
+                            <option value="lufthansa">Lufthansa</option>
+                            <option value="ryanair">Ryanair</option>
+                            <option value="united">United</option>
+                        </select><br><br>
+                    </div>
 
-            <label for="pren">Date de naissance : </label><br>
-            <input type="date" id="date" name="date" required><br><br>
-
-            <label for="mail">Adresse e-mail personnelle : </label><br>
-            <input type="text" name="mail" size="50"><br><br>
-
-            <label for="mailpro">Adresse e-mail professionnelle : </label><br>
-            <input type="text" name="mailpro" size="50"><br><br>
-
-            <label for="cpgn">Compagnie aérienne : </label><br>
-            <select>
-                <option value="airchina">Air China</option>
-                <option value="aircorse">Air Corsica</option>
-                <option value="airfrance">Air France</option>
-                <option value="american">American Airlines</option>
-                <option value="easy">Easy Jet</option>
-                <option value="emirates">Emirates</option>
-                <option value="iberia">Iberia</option>
-                <option value="klm">KLM</option>
-                <option value="lufthansa">Lufthansa</option>
-                <option value="ryanair">Ryanair</option>
-                <option value="united">United</option>
-            </select><br><br>
-
-            <label for="mailpro">Autre : </label><br>
-            <input type="text" name="mailpro" size="40"><br><br>
-
-            <label for="fonction">Fonction ou poste occupé</label><br>
-            <input type="text" name="fonction" size="60"><br><br>
-
-            <label for="demande">Formulez votre demande ici : </label><br />
-            <textarea name="demande" id="demande" rows="5" cols="60"></textarea><br><br><br>
-
+                    <div class="champs2">
+                        <label for="mailpro">Autre : </label>
+                        <input type="text" name="mailpro" size="30"><br><br>
+                    </div>
+                    <div class="champs2">
+                        <label for="fonction">Fonction ou poste occupé : </label><br>
+                        <input type="text" name="fonction" size="40"><br><br>
+                    </div>
+                    <div class="champs2">
+                        <label for="demande">Formulez votre demande ici : </label><br /><br>
+                        <textarea name="demande" id="demande" rows="5" cols="60"></textarea><br>
+                    </div>
+                </div>
+            </div>
             <div class="end">
                 <a href="#" onclick="resetForm();" id="reinit" type="reset">Réinitialiser</a>
                 <a href="#" id="valider" type="submit">Envoyer</a><br>
             </div>
             
         </form>
-    </div>
 </section>
 
 <script type="text/javascript">
